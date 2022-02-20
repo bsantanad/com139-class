7efd7a9a99c7aa13ec48a299aa03700ed20c8d20
bsantanad <0211518@up.edu.mx>
2022-02-20 17:24:49 -0600

    game can now be read from a config file
    you can use config files to save your configuartions
    also iterations had a bug, now it works perfectly :)
    
    Signed-off-by: bsantanad <0211518@up.edu.mx>

--- GoL/conway.py
+++ GoL/conway.py
@@ -13,7 +13,7 @@ ON = 255
 OFF = 0
 vals = [ON, OFF]
 
-def randomGrid(N):
+def random_grid(N):
     """returns a grid of NxN random values"""
     return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
 
@@ -27,6 +27,8 @@ def main():
 
     parser.add_argument('-s', '--size', dest = 'N', required = False)
     parser.add_argument('-i', '--iterations', dest = 'iter', required = False)
+    parser.add_argument('-f', '--config-file',
+                        dest = 'filename', required = False)
     args = parser.parse_args()
 
     # set grid size
@@ -36,35 +38,105 @@ def main():
             raise argparse.ArgumentTypeError('min grid is 8x8')
         N = int(args.N)
 
+
+
+    # read file and parse the info
+    coords = []
+    if args.filename:
+        filename = args.filename
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            # FIXME make the width and height do something
+            (width, height), iterations, coords = parse_lines(lines)
+
+    grid = np.zeros((width, height))
+
+    if len(coords) < 0:
+        grid = random_grid(N)
+
+    for x, y in coords:
+        grid[x][y] = ON
+
     iterations = None
     if args.iter:
         if int(args.iter) < 0:
             raise argparse.ArgumentTypeError('cannot have negative iterations')
         iterations = int(args.iter)
 
-
     # set animation update interval
     update_interval = 50
-
-    # declare grid
-    grid = np.array([])
-    # populate grid with random on/off - more off than on
-    grid = randomGrid(N)
 
     # set up animation
     fig, ax = plt.subplots()
     img = ax.imshow(grid, interpolation = 'nearest')
     ani = anim_c(
-        fig, img, grid, N,
+        fig, img, grid, width, height,
         update_interval,
         max_iterations = iterations
     )
 
     plt.show()
 
+def parse_lines(lines):
+    '''
+    parse text file lines, we have to go to add all the if's becuase
+    the user might want to add comments to the file. But at the end of
+    the day we just care for three things:
+
+    :returns tuple: (width, height)
+    :returns generations: int
+    :returns list: tuple list width coord x and y
+
+    So at the end it will return something like this
+    return ((width, height), generations, [(1, 3), (4, 4)])
+    '''
+    # get width and height
+    if '#' in lines[0]:
+        width, height, _ = lines[0].split(' ', 2)
+    else:
+        width, height = lines[0].split(' ', 1)
+
+    # get generations
+    if '#' in lines[1]:
+        generations, _ = lines[1].split(' ', 1)
+    else:
+        generations = lines[1]
+
+    try:
+        width = int(width)
+        height = int(height)
+        generations = int(generations)
+    except ValueError:
+        raise RuntimeError('bad config file, please refer to config to check'
+                           ' how to fix it')
+
+    coords = []
+    for i, line in enumerate(lines):
+        if len(line) < 2:
+            continue
+        if i < 2:
+            continue
+        line = line.strip()
+        try:
+            if '#' in line:
+                x, y, _ = line.split(' ', 2)
+            else:
+                x, y = line.split(' ')
+        except ValueError:
+            print('bad config file, please refer to config to check how'
+                  f' to fix it. (check line {i})')
+            raise
+        if not x.isnumeric() or not y.isnumeric():
+            raise RuntimeError('some of the coords you wrote in the config'
+                  f' file are not numeric, please fix this. (check line {i})')
+        coords.append((int(x), int(y)))
+
+    return ((width, height), generations, coords)
+
+
 class anim_c:
     max_iterations = 200
-    def __init__(self, fig, img, grid, N,
+    def __init__(self, fig, img, grid, width, height,
                  update_interval = 50, max_iterations = None):
 
         if max_iterations:
@@ -72,39 +144,38 @@ class anim_c:
         self.iterations = 0
 
         self.ani = animation.FuncAnimation(
-            fig, self.update, fargs = (img, grid, N),
-            frames = 50,
-            interval = update_interval,
-            save_count = 50,
+            fig, self.update, fargs = (img, grid, width, height),
+            frames = 60,
         )
 
-    def update(self, frameNum, img, grid, N):
+    def update(self, frameNum, img, grid, width, height):
 
         # check iterations, if limit
         self.iterations += 1
         if self.iterations > self.max_iterations:
-            print(self.iterations)
             self.ani.event_source.stop()
 
         # copy grid since we require 8 neighbors for calculation
         # and we go line by line
         new_grid = grid.copy()
-        for i in range(N):
-            for j in range(N):
+        for i in range(width):
+            for j in range(height):
 
-                # we have to calc the total amount of `ON` neighbors, we check them
-                # by index, using the `%N` for it to continue on the next side of
-                # the board when we reached a side. (like the snake game when
-                # reaching the walls). Then we have to divide by 255, this is
-                # because the values in the grid are 0 or 255, and we are looking
-                # how many neighbors the cell has, so we want to change that 255 to
-                # 1
+                # we have to calc the total amount of `ON` neighbors, we check
+                # them by index, using the `%N` for it to continue on the next
+                # side of the board when we reached a side. (like the snake
+                # game when reaching the walls). Then we have to divide by 255,
+                # this is because the values in the grid are 0 or 255, and we
+                # are looking how many neighbors the cell has, so we want to
+                # change that 255 to 1
                 total =int(
                     (
-                        grid[i, (j-1)%N] + grid[i, (j+1)%N] + # up/down neigh
-                        grid[(i-1)%N, j] + grid[(i+1)%N, j] + # left/right neigh
-                        grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + #diagonal
-                        grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]
+                        grid[i, (j-1)% height] + grid[i, (j+1)% height] +
+                        grid[(i-1)% width, j] + grid[(i+1)% width, j] +
+                        grid[(i-1)% width, (j-1)% height] +
+                        grid[(i-1)% width, (j+1)% height] +
+                        grid[(i+1)% width, (j-1)% height] +
+                        grid[(i+1)% width, (j+1)% height]
                     )
                     /255
                 )
